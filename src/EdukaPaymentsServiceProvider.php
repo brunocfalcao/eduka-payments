2413da9f63e805a123555dbe5feb0833e9988982
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2021-08-04 11:19:05 +0200

    Initial commit

---
+++ src/EdukaPaymentsServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Eduka\Payments;
+
+use Illuminate\Support\ServiceProvider;
+
+final class EdukaPaymentsServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
