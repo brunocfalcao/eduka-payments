4c795fd809c80e112b81b014ebf7c92ce78c228d
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2021-09-01 00:45:31 +0200

    wip

--- src/EdukaPaymentsServiceProvider.php
+++ src/EdukaPaymentsServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace Eduka\Payments;
 
+use Eduka\Payments\Listeners\OnboardUser;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use ProtoneMedia\LaravelPaddle\Events\PaymentSucceeded;
 
 final class EdukaPaymentsServiceProvider extends ServiceProvider
 {
@@ -13,6 +17,25 @@ final class EdukaPaymentsServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        //
+        $this->loadRoutes();
+        $this->loadEvents();
+    }
+
+    protected function loadEvents()
+    {
+        Event::listen(
+            PaymentSucceeded::class,
+            [OnboardUser::class, 'handle']
+        );
+    }
+
+    protected function loadRoutes()
+    {
+        $routesPath = __DIR__.'/../routes/web.php';
+
+        Route::middleware('web')
+        ->group(function () use ($routesPath) {
+            include $routesPath;
+        });
     }
 }
---
+++ src/Listeners/OnboardUser.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Eduka\Payments\Listeners;
+
+class OnboardUser
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle($event)
+    {
+        //
+    }
+}
