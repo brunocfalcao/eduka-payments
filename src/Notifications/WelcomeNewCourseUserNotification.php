df82bde395bcdc6641e35b0d68a6b18d92684aee
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2023-08-13 22:02:43 +0200

    Add welcome notification

---
+++ src/Notifications/WelcomeNewCourseUserNotification.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Eduka\Payments\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class WelcomeNewCourseUserNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(public string $courseName)
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        // @todo update message
+        return (new MailMessage)
+                    ->line('New Course Email Welcome to ' . $this->courseName)
+                    ->action('Set password', url('/'))
+                    ->line('We welcome you to this course, kindly use the reset password feature to set a new password!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
