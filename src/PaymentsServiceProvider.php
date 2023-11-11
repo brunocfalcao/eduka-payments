6573995723b5216aa6e8a256ba955c1ee3adaafc
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-11-11 21:29:18 +0100

    Update PaymentsServiceProvider.php

--- src/PaymentsServiceProvider.php
+++ src/PaymentsServiceProvider.php
@@ -3,7 +3,6 @@
 namespace Eduka\Payments;
 
 use Eduka\Abstracts\Classes\EdukaServiceProvider;
-use Eduka\Analytics\Middleware\TrackVisit;
 use Eduka\Payments\Commands\SyncPurchasePowerParity;
 use Illuminate\Support\Facades\Route;
 
@@ -35,7 +34,6 @@ class PaymentsServiceProvider extends EdukaServiceProvider
 
         Route::middleware([
             'web',
-            TrackVisit::class,
         ])
         ->group(function () use ($routesPath) {
             include $routesPath;
