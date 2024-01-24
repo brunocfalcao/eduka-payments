c8618bccc3557ce4063dd14c5b7848d573122c9d
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2024-01-24 08:53:57 +0100

    Update PaymentController.php

--- src/Http/Controllers/PaymentController.php
+++ src/Http/Controllers/PaymentController.php
@@ -5,20 +5,15 @@ namespace Eduka\Payments\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Brunocfalcao\Cerebrus\Cerebrus;
 use Brunocfalcao\Tokenizer\Models\Token;
-use Eduka\Cube\Actions\Coupon\FindCoupon;
-use Eduka\Cube\Models\Coupon;
 use Eduka\Cube\Models\Course;
 use Eduka\Cube\Models\Order;
 use Eduka\Cube\Models\Variant;
 use Eduka\Nereus\Facades\Nereus;
-use Eduka\Payments\Actions\LemonSqueezyCoupon;
 use Eduka\Payments\PaymentProviders\LemonSqueezy\LemonSqueezy;
 use Eduka\Payments\PaymentProviders\LemonSqueezy\Responses\CreatedCheckoutResponse;
 use Exception;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Http\Request as HttpRequest;
-use Illuminate\Support\Facades\Log;
 
 class PaymentController extends Controller
 {
@@ -51,9 +46,7 @@ class PaymentController extends Controller
             return redirect()->back();
         }
 
-        $this->variant = $this->course->getVariantOrDefault(
-            Variant::firstWhere('uuid', $request->input('variant'))
-        );
+        $this->variant = Variant::firstWhere('uuid', $request->input('variant'));
 
         $userCountry ??= request()->header('cf-ipcountry');
 
@@ -83,7 +76,7 @@ class PaymentController extends Controller
             // Validates and burns token.
             $this->validateWebhookToken();
 
-            // The variant UUID should be the same as the LS varint id instance.
+            // The variant UUID should be the same as the LS variant id instance.
             $this->checkVariantUuid();
 
             // Store the order and start the course assignment process.
@@ -184,13 +177,14 @@ class PaymentController extends Controller
                 ]);
 
             // Conditionally applying setCustomPrice.
-            if ($variant->priceOverrideInCents()) {
+            if ($variant->lemon_squeezy_price_override) {
                 $responseString = $responseString
                     ->setCustomPrice(
                         $variant->lemon_squeezy_price_override * 100
                     );
             }
 
+            // Eager load the course.
             $variant = Variant::with('course')->find($variant->id);
 
             $responseString = $responseString
@@ -206,81 +200,7 @@ class PaymentController extends Controller
 
             return $raw;
         } catch (Exception $e) {
-            $this->log('could not create checkout', $e);
-            throw $e;
+            throw new Exception('could not create checkout - ' . $e->getMessage());
         }
-    }
-
-    /**
-     * @aryan:
-     * So, the logic should be:
-     * 1. Visitor visits website.
-     * 2. You grab the country header (already on the codebase).
-     * 3. You check if you have the coupon created on our local
-     *    coupons table. If not:
-     *    3.1. You create a global coupon on the lemonsqueezy,
-     *         but please trigger an async job queue=default
-     *         because I don't the website to slow down because
-     *         of that api request (can take sometime I guess).
-     *    3.2. You add the coupon to our eduka database. No need
-     *         to have a "per course" logic, because these are
-     *         global product-wide coupons. You check the PPP.
-     * 4. You always show on the website the coupon in case
-     *    the courses.enable_purchase_power_parity = true. If
-     *    false, we don't show the coupon (although, if they
-     *    know how to use it, they can use it surely).
-     *
-     * At the end, you just add coupons as long as new visitors
-     * countries are arriving. We don't create them all at once.
-     */
-    protected function ensureCouponOnLemonSqueezy(string $country): void
-    {
-        $coupon = FindCoupon::fromCountryRecord($country, Nereus::course()->id);
-
-        // coupon does not exist in database
-        if (! $coupon) {
-            return;
-        }
-
-        // check if coupon has remote reference id, if yes, it means coupon also exists in lemon squeezy
-        if ($coupon->hasRemoteReference()) {
-            return;
-        }
-
-        // reaching here means coupon exists in database, but not on lemon squeezy.
-        // create coupon on lemon squeezy and update remote reference id
-        $code = $coupon->generateCodeForCountry(strtoupper($country->getName()), strtoupper($country->getIsoCode()));
-
-        $couponApi = new LemonSqueezy($this->lemonSqueezyApiKey);
-
-        $reference = (new LemonSqueezyCoupon)->create(
-            $couponApi,
-            $this->course->lemon_squeezy_store_id,
-            $code,
-            $coupon->discount_amount,
-            $coupon->is_flat_discount,
-        );
-
-        if (! $reference) {
-            // could not create coupon in lemon squezzy
-            return;
-        }
-
-        // coupon created, update $coupon in local db
-        $coupon->update([
-            'code' => $code,
-            'remote_reference_id' => $reference,
-        ]);
-    }
-
-    protected function log(string $message, ?Exception $e, array $data = [])
-    {
-        if ($e) {
-            $data[] = [
-                'message' => $e->getMessage(),
-            ];
-        }
-
-        Log::error($message, $data);
     }
 }
