3faf407605117f72c6fa13c7cecff74252fade86
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2024-05-28 14:51:43 +0200

    Update SimulateWebhook.php

--- src/Commands/SimulateWebhook.php
+++ src/Commands/SimulateWebhook.php
@@ -34,105 +34,105 @@ class SimulateWebhook extends Command
         // Create the token that will be called by the json below.
         Token::createToken('s3Exo2NJvjFj1tH1nw4aktPSOIXuR3qjnRdenpnk');
 
-        $url = 'http://brunofalcao-dev.local:8000/lemonsqueezy/webhook';
+        $url = 'http://eduka-pro.local:8000/lemonsqueezy/webhook';
 
         $variantId = env('MNOR_VARIANT_ID');
 
         $webhook = '
 {
   "data": {
-    "id": "2490206",
+    "id": "2773413",
     "type": "orders",
     "links": {
-      "self": "https://api.lemonsqueezy.com/v1/orders/2490206"
+      "self": "https://api.lemonsqueezy.com/v1/orders/2773413"
     },
     "attributes": {
       "tax": 0,
       "urls": {
-        "receipt": "https://app.lemonsqueezy.com/my-orders/2fc92a86-e390-4244-9125-fbd666b648dd?signature=23e820e0cade490b574295bce7c81c42c78e6972e64f94fe4f47d655407ed09c"
+        "receipt": "https://app.lemonsqueezy.com/my-orders/2749f9aa-434a-41f8-ad12-1ee7d2bc449d?signature=eed27fdfaf81a2cbc2f3658ad1f3997013202b79d5be29ecdbfad91f3b33c04e"
       },
-      "total": 0,
+      "total": 100,
       "status": "paid",
       "tax_usd": 0,
       "currency": "USD",
       "refunded": false,
       "store_id": 25678,
-      "subtotal": 0,
+      "subtotal": 100,
       "tax_name": "",
       "tax_rate": 0,
       "setup_fee": 0,
       "test_mode": true,
-      "total_usd": 0,
+      "total_usd": 100,
       "user_name": "Bruno Falcao",
-      "created_at": "2024-04-15T07:21:43.000000Z",
-      "identifier": "2fc92a86-e390-4244-9125-fbd666b648dd",
-      "updated_at": "2024-04-15T07:21:44.000000Z",
+      "created_at": "2024-05-27T19:38:00.000000Z",
+      "identifier": "2749f9aa-434a-41f8-ad12-1ee7d2bc449d",
+      "updated_at": "2024-05-27T19:38:00.000000Z",
       "user_email": "bruno.falcao@live.com",
       "customer_id": 1628498,
       "refunded_at": null,
-      "order_number": 2567853,
-      "subtotal_usd": 0,
+      "order_number": 2567855,
+      "subtotal_usd": 100,
       "currency_rate": "1.00000000",
       "setup_fee_usd": 0,
       "tax_formatted": "$0.00",
       "tax_inclusive": false,
       "discount_total": 0,
-      "total_formatted": "$0.00",
+      "total_formatted": "$1.00",
       "first_order_item": {
-        "id": 2450871,
-        "price": 0,
-        "order_id": 2490206,
+        "id": 2731768,
+        "price": 100,
+        "order_id": 2773413,
         "price_id": 206098,
         "quantity": 1,
         "test_mode": true,
-        "created_at": "2024-04-15T07:21:44.000000Z",
+        "created_at": "2024-05-27T19:38:00.000000Z",
         "product_id": 154629,
-        "updated_at": "2024-04-15T07:21:44.000000Z",
+        "updated_at": "2024-05-27T19:38:00.000000Z",
         "variant_id": 192214,
         "product_name": "Mastering Nova - Orion",
         "variant_name": "Default"
       },
       "status_formatted": "Paid",
       "discount_total_usd": 0,
-      "subtotal_formatted": "$0.00",
+      "subtotal_formatted": "$1.00",
       "setup_fee_formatted": "$0.00",
       "discount_total_formatted": "$0.00"
     },
     "relationships": {
       "store": {
         "links": {
-          "self": "https://api.lemonsqueezy.com/v1/orders/2490206/relationships/store",
-          "related": "https://api.lemonsqueezy.com/v1/orders/2490206/store"
+          "self": "https://api.lemonsqueezy.com/v1/orders/2773413/relationships/store",
+          "related": "https://api.lemonsqueezy.com/v1/orders/2773413/store"
         }
       },
       "customer": {
         "links": {
-          "self": "https://api.lemonsqueezy.com/v1/orders/2490206/relationships/customer",
-          "related": "https://api.lemonsqueezy.com/v1/orders/2490206/customer"
+          "self": "https://api.lemonsqueezy.com/v1/orders/2773413/relationships/customer",
+          "related": "https://api.lemonsqueezy.com/v1/orders/2773413/customer"
         }
       },
       "order-items": {
         "links": {
-          "self": "https://api.lemonsqueezy.com/v1/orders/2490206/relationships/order-items",
-          "related": "https://api.lemonsqueezy.com/v1/orders/2490206/order-items"
+          "self": "https://api.lemonsqueezy.com/v1/orders/2773413/relationships/order-items",
+          "related": "https://api.lemonsqueezy.com/v1/orders/2773413/order-items"
         }
       },
       "license-keys": {
         "links": {
-          "self": "https://api.lemonsqueezy.com/v1/orders/2490206/relationships/license-keys",
-          "related": "https://api.lemonsqueezy.com/v1/orders/2490206/license-keys"
+          "self": "https://api.lemonsqueezy.com/v1/orders/2773413/relationships/license-keys",
+          "related": "https://api.lemonsqueezy.com/v1/orders/2773413/license-keys"
         }
       },
       "subscriptions": {
         "links": {
-          "self": "https://api.lemonsqueezy.com/v1/orders/2490206/relationships/subscriptions",
-          "related": "https://api.lemonsqueezy.com/v1/orders/2490206/subscriptions"
+          "self": "https://api.lemonsqueezy.com/v1/orders/2773413/relationships/subscriptions",
+          "related": "https://api.lemonsqueezy.com/v1/orders/2773413/subscriptions"
         }
       },
       "discount-redemptions": {
         "links": {
-          "self": "https://api.lemonsqueezy.com/v1/orders/2490206/relationships/discount-redemptions",
-          "related": "https://api.lemonsqueezy.com/v1/orders/2490206/discount-redemptions"
+          "self": "https://api.lemonsqueezy.com/v1/orders/2773413/relationships/discount-redemptions",
+          "related": "https://api.lemonsqueezy.com/v1/orders/2773413/discount-redemptions"
         }
       }
     }
@@ -140,9 +140,10 @@ class SimulateWebhook extends Command
   "meta": {
     "test_mode": true,
     "event_name": "order_created",
-    "webhook_id": "c9a15f05-ccf0-4af7-8eba-6bccb330e371",
+    "webhook_id": "56baa80d-8843-4e2a-8af6-bbb5abcdeb85",
     "custom_data": {
-      "token": "s3Exo2NJvjFj1tH1nw4aktPSOIXuR3qjnRdenpnk"
+      "token": "s3Exo2NJvjFj1tH1nw4aktPSOIXuR3qjnRdenpnk",
+      "country": "CH"
     }
   }
 }
